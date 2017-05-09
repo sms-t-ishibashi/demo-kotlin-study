191391c62ee3d7d23e87526c8823438c35720825
sms-t-ishibashi <tomoharu-ishibashi@bm-sms.co.jp>
2017-05-09 15:48:04 +0900

    implements controller

--- src/main/kotlin/kotolin/study/DemokotolinApplication.kt
+++ src/main/kotlin/kotolin/study/DemokotolinApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class DemokotolinApplication {
+class DemokotolinApplication {
 
     companion object {
         @JvmStatic
--- src/main/kotlin/kotolin/study/controller/page/MemoController.kt
+++ src/main/kotlin/kotolin/study/controller/page/MemoController.kt
@@ -1,4 +1,4 @@
-package controller.page
+package kotolin.study.controller.page
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -14,24 +14,36 @@ class MemoController {
     @RequestMapping("")
     fun get(model: Model): String {
 //        List<Map<String, Object>> items = new ArrayList<>();
-        val items = arrayListOf<Map<String, Any>>()
+        val items = mutableListOf<MutableMap<String, Any>>()
 //        val items2 = ArrayList<Map<String, Any>>()
 //        val items3 = mutableListOf<Map<String, Any>>()
 
 //        Map<String, Object> item = new HashMap<>();
-        val item = HashMap<String, Any>()
-//        val item2 = hashMapOf<String, Any>()
-//        val item3 = mutableMapOf<String, Any>()
+        val item = LinkedHashMap<String, Any>()
+        val item2 = hashMapOf<String, Any>()
+        val item3 = mutableMapOf<String, Any>()
 
 //        item.put("memo", "Empty Memo");
 //        item.put("author", "Empty Author");
 //        items.add(item);
-        item.put("memo", "Empty")
+
+        // Javaの記述
+        item.put("memo", "Empty Memo")
         item.put("author", "Empty Author")
         items.add(item)
 
+        // スクリプト言語っぽい記述
+        item2["memo"] = "memo memo"
+        item2["author"] = "Author bassy"
+        items += item2
+
+        // Kotlinの(?)記述
+        item3 += ("memo" to "memo memo memo")
+        item3 += ("author" to "issy bassy gakky")
+        items += item3
+
 //        model.addAttribute("items", items);
-        model.addAttribute(items)
+        model.addAttribute("items", items)
         return "memo"
     }
 }
