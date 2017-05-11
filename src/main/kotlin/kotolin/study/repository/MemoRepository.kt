40010dd42d7c5d961cd3e431a495412f521e5076
sms-t-ishibashi <tomoharu-ishibashi@bm-sms.co.jp>
2017-05-11 10:38:06 +0900

    add MemoRepository

---
+++ src/main/kotlin/kotolin/study/repository/MemoRepository.kt
@@ -0,0 +1,13 @@
+package kotolin.study.repository
+
+import kotolin.study.model.Memo
+import org.springframework.stereotype.Repository
+
+/**
+ * @author tomoharu-ishibashi
+ */
+interface MemoRepository {
+    fun find(): List<Memo>
+    fun findByAuthor(author: String): List<Memo>
+    fun save(item: Memo)
+}
---
+++ src/main/kotlin/kotolin/study/repository/jdbc/JdbcMemoRepository.kt
@@ -0,0 +1,34 @@
+package kotolin.study.repository.jdbc
+
+import kotolin.study.model.Memo
+import kotolin.study.repository.MemoRepository
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Repository
+
+
+/**
+ * @author tomoharu-ishibashi
+ */
+@Repository
+class JdbcMemoRepository(val jdbcTemplate: JdbcTemplate) : MemoRepository {
+    override fun find(): List<Memo> {
+        return jdbcTemplate.query(
+                "SELECT MEMO, AUTHOR FROM MEMO ORDER BY CREATED ASC",
+                { resultSet, i ->
+                    Memo(resultSet.getString("MEMO"), resultSet.getString("AUTHOR"), null)
+                })
+    }
+
+    override fun findByAuthor(author: String): List<Memo> {
+        return jdbcTemplate.query(
+                "SELECT MEMO, AUTHOR FROM MEMO WHERE AUTHOR = ? ORDER BY CREATED ASC",
+                { resultSet, i -> Memo(resultSet.getString("MEMO"), resultSet.getString("AUTHOR"), null) },
+                arrayOf(author))
+    }
+
+    override fun save(item: Memo) {
+        jdbcTemplate.update(
+                "INSERT INTO MEMO (MEMO, AUTHOR, CREATED) VALUES (?, ?, CURRENT_TIMESTAMP)",
+                item.memo, item.author);
+    }
+}
